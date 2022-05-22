bb95cc4828b4e00b53d1181f342288b1ca39920b
Amax <andreas.amaxaris@gmail.com>
2022-05-22 23:16:51 +0300

    well?

--- SuffixTrie/src/suffixtrie/main_GUI.java
+++ SuffixTrie/src/suffixtrie/main_GUI.java
@@ -50,6 +50,7 @@ public class main_GUI extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setToolTipText("");
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
